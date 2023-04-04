7832c99f01e8d63c0c49237325ffb65df0f2f6a1
Gino <ginociancia10@gmail.com>
2023-04-03 23:23:53 -0300

    deploy done

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://ginogc01.github.io',
+    base: 'D.M-Terapia-Emocional',
     build: {
         assets: 'assets'
       }
